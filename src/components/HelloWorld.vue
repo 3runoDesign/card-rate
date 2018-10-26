27d54b4acfd287d2731f1ac552c1f0461b83e2b7
Bruno Fernando <bruno2fernando@gmail.com>
2018-10-26 01:13:26 -0300

    Add fields

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,86 +1,72 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+<div class="">
+  <section class="hero is-success is-fullheight">
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <h1 class="title">
+        Precificar produto ou serviço
+      </h1>
+
+      <b-field label="Valor do produto/serviço">
+
+        <div class="control">
+          <money class="input" v-model="valor_total" v-bind="money"></money>
+        </div>
+
+      </b-field>
+
+      <b-field label="Valor do desconto">
+        <b-input type="number" v-model="desconto"
+          icon-pack="fas"
+          icon="percent"></b-input>
+      </b-field>
+
+      <b-field label="Qual o plano de recebimento?">
+        <b-select placeholder="Selecione o plano de recebimento"
+        v-model="juros_selected">
+            <option
+                v-for="(tarifa, key) in tarifas"
+                :value="key"
+                :key="key">
+                {{ tarifa.dias }} dias
+            </option>
+        </b-select>
+      </b-field>
+
+    <button class="button is-primary is-medium"
+        @click="isCardModalActive = true">
+        Gerar valor
+    </button>
+
+    </div>
   </div>
+  </section>
+
+  <b-modal :active.sync="isCardModalActive" :width="640" scroll="keep">
+      <div class="card">
+          <div class="card-content">
+              <div class="content">
+
+                  <div class="notification is-primary">
+                    <h1 class="title">R$ {{ (parseFloat(valor_total).toFixed(2)).toLocaleString('pt-BR') }}</h1>
+                    <h2 class="subtitle">Valor normal sem desconto</h2>
+                  </div>
+
+                  <div class="notification is-link" v-if="desconto">
+                    <h1 class="title">R$ {{ (parseFloat(valor_total - (valor_total * (desconto/100) )).toFixed(2)).toLocaleString('pt-BR') }}</h1>
+                    <h2 class="subtitle">Valor com desconto de {{ desconto }}%</h2>
+                  </div>
+
+                  <div class="notification is-info">
+
+                    <h1 class="title"><i class="fab fa-cc-visa"></i> R$ {{ (parseFloat((valor_total - (valor_total * (desconto/100) )) / (1 - parseFloat((tarifas[juros_selected].juros / 100)))).toFixed(2)).toLocaleString('pt-BR') }}</h1>
+                    <h2 class="subtitle">Valor para parcelamento</h2>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </b-modal>
+</div>
 </template>
 
 <script>
@@ -88,26 +74,44 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      isCardModalActive: false,
+
+      valor_total: 2403,
+      desconto: 0,
+      valor_receber: 0,
+
+      juros_selected: 2,
+      tarifas: [
+        {
+          dias: 1,
+          juros: 5.59
+        },
+        {
+          dias: 14,
+          juros: 4.59
+        },
+        {
+          dias: 30,
+          juros: 3.79
+        },
+      ],
+
+      money: {
+        decimal: ',',
+        thousands: '.',
+        prefix: 'R$ ',
+        precision: 2,
+        masked: false
+      }
     }
+  },
+  methods: {
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 
+import VueTheMask from 'vue-the-mask'
+
+import money from 'v-money'
+
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(VueTheMask)
+Vue.use(money, {precision: 4})
+Vue.use(Buefy)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
